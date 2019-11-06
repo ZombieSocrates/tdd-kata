a794fb124938e3a58af119507bd7ad9011a1086d
Ross Weijer <ross@datascopeanalytics.com>
2019-11-06 12:03:09 -0600

    JS Implementation - Second Test Written and Auto-Passes

--- in-javascript/test.js
+++ in-javascript/test.js
@@ -8,11 +8,20 @@ let BowlingGame = games.Game;
 describe("Bowling Scorer", () => {
 	test("it should score a gutterball game as zero", () => {
         let g = new BowlingGame();
-        for (let i = 0; i < 20; i ++) {
+        for (let i = 0; i < 20; i++) {
         	g.roll(0)
         }
         let gutterScore = 0;
-        expect(g.scoreGame()).toEqual(gutterScore)
+        expect(g.scoreGame()).toEqual(gutterScore);
       
+	})
+
+	test("it should score all ones game as 20", () => {
+		let g = new BowlingGame();
+		for (let i = 0; i < 20; i++) {
+			g.roll(1)
+		}
+		let allOnesScore = 20;
+		expect(g.scoreGame()).toEqual(allOnesScore);
 	})
 })
