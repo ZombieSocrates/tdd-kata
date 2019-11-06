d2ffa33fbf4ab295973889bf08f64a9309a0cdfb
Ross Weijer <ross@datascopeanalytics.com>
2019-11-06 17:21:00 -0600

    JS Implementation - refactor bowling.js, keep passing coverage on all 5 tests

--- in-javascript/bowling.js
+++ in-javascript/bowling.js
@@ -20,32 +20,52 @@ class BowlingGame {
         this.rollMany(pins = 5, times = 2);
     };
 
-    isStrike(){
-        return this.ballRolls[this.scoreIndex] == 10;
-    }
-
     scoreFrame() {
+        if (this.isStrike()) {
+            return this.ballRolls[this.scoreIndex]
+        }
         let nextIndex = this.scoreIndex + 1;
         return this.ballRolls[this.scoreIndex] + this.ballRolls[nextIndex];
     };
 
+    isStrike(){
+        return this.ballRolls[this.scoreIndex] == 10;
+    };
+
+    strikeBonus(){
+        let startRoll = this.scoreIndex + 1;
+        let endRoll = startRoll + 2;
+        let bonuses = this.ballRolls.slice(startRoll, endRoll);
+        return bonuses.reduce((a,b) => a + b, 0);
+    };
+
+    isSpare(){
+        return (!this.isStrike()) && (this.scoreFrame() == 10);
+    };
+
+    spareBonus(){
+        return this.ballRolls[this.scoreIndex + 2];
+    };
+
+    incrementScoreIndex() {
+        let incr = this.isStrike() ? 1 : 2;
+        this.scoreIndex += incr;
+    };
+
     scoreGame() {
         for (let i = 0; i < this.nFrames; i++){
-            if (this.isStrike()){
-                this.score += this.ballRolls.slice(this.scoreIndex, this.scoreIndex + 3)
-                                            .reduce((a,b) => a + b, 0)
-                this.scoreIndex += 1
-                continue                            
-            }
             this.score += this.scoreFrame()
-            if (this.scoreFrame() == 10) {
-                this.score += this.ballRolls[this.scoreIndex + 2]
+            if (this.isStrike()){
+                this.score += this.strikeBonus();                           
             }
-            this.scoreIndex += 2
+            if (this.isSpare()) {
+                this.score += this.spareBonus();
+            }
+            this.incrementScoreIndex()
         }
         return this.score;
     };
-}
+};
 
 
 module.exports = {
