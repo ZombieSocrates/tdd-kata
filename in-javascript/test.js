97b857b83464248b1c74b3fc98ae6e4a59ba2422
Ross Weijer <ross@datascopeanalytics.com>
2019-11-06 15:33:27 -0600

    JS Implementation - fourth test fails ... scorer weirdly returns NaN.

--- in-javascript/test.js
+++ in-javascript/test.js
@@ -6,28 +6,37 @@ let BowlingGame = games.Game;
 let currGame;
 
 beforeEach( () => {
-	currGame = new BowlingGame();
+    currGame = new BowlingGame();
 })
 
 describe("Bowling Scorer", () => {
-	test("it should score a gutterball game as zero", () => {
+    test("it should score a gutterball game as zero", () => {
         currGame.rollMany(pins = 0, times = 20);
         let gutterScore = 0;
         expect(currGame.scoreGame()).toEqual(gutterScore);
       
-	})
+    })
 
-	test("it should score all ones game as 20", () => {
-		currGame.rollMany(pins = 1, times = 20);
-		let allOnesScore = 20;
-		expect(currGame.scoreGame()).toEqual(allOnesScore);
-	})
+    test("it should score all ones game as 20", () => {
+        currGame.rollMany(pins = 1, times = 20);
+        let allOnesScore = 20;
+        expect(currGame.scoreGame()).toEqual(allOnesScore);
+    })
 
-	test("it should score a spare properly", () => {
-		currGame.rollSpare();
-		currGame.roll(3);
-		currGame.rollMany(pins =0, times = 17);
-		let spareGameScore = 16;
-		expect(currGame.scoreGame()).toEqual(spareGameScore);
-	})
+    test("it should score a spare properly", () => {
+        currGame.rollSpare();
+        currGame.roll(3);
+        currGame.rollMany(pins =0, times = 17);
+        let spareGameScore = 16;
+        expect(currGame.scoreGame()).toEqual(spareGameScore);
+    })
+
+    test("it should score a strike properly", () => {
+        currGame.roll(10);
+        currGame.roll(3);
+        currGame.roll(4);
+        currGame.rollMany(pins = 0, times = 16);
+        let strikeGameScore = 24;
+        expect(currGame.scoreGame()).toEqual(strikeGameScore);
+    })
 })
