037765e79bfd4ca21a1903f6d145280c38f03776
Ross Weijer <ross@datascopeanalytics.com>
2019-11-06 15:35:28 -0600

    JS Implementation - fourth test succeeds ... Still a bit brittle

--- in-javascript/bowling.js
+++ in-javascript/bowling.js
@@ -1,44 +1,53 @@
 class BowlingGame {
-	constructor() {
-		this.ballRolls = [];
-		this.score = 0;
-		this.nFrames = 10;
-		this.scoreIndex = 0;
-	};
+    constructor() {
+        this.ballRolls = [];
+        this.score = 0;
+        this.nFrames = 10;
+        this.scoreIndex = 0;
+    };
 
-	roll(pins) {
-		this.ballRolls.push(pins);
-	};
+    roll(pins) {
+        this.ballRolls.push(pins);
+    };
 
-	rollMany(pins, times) {
-		for (let i = 0; i < times; i++) {
-			this.roll(pins);
-		}
-	};
+    rollMany(pins, times) {
+        for (let i = 0; i < times; i++) {
+            this.roll(pins);
+        }
+    };
 
-	rollSpare(){
-		this.rollMany(pins = 5, times = 2);
-	};
+    rollSpare(){
+        this.rollMany(pins = 5, times = 2);
+    };
 
-	scoreFrame() {
-		let nextIndex = this.scoreIndex + 1;
-		return this.ballRolls[this.scoreIndex] + this.ballRolls[nextIndex];
-	};
+    isStrike(){
+        return this.ballRolls[this.scoreIndex] == 10;
+    }
 
-	scoreGame() {
-		for (let i = 0; i < this.nFrames; i++){
-			this.score += this.scoreFrame()
+    scoreFrame() {
+        let nextIndex = this.scoreIndex + 1;
+        return this.ballRolls[this.scoreIndex] + this.ballRolls[nextIndex];
+    };
+
+    scoreGame() {
+        for (let i = 0; i < this.nFrames; i++){
+            if (this.isStrike()){
+                this.score += this.ballRolls.slice(this.scoreIndex, this.scoreIndex + 3)
+                                            .reduce((a,b) => a + b, 0)
+                this.scoreIndex += 1
+                continue                            
+            }
+            this.score += this.scoreFrame()
             if (this.scoreFrame() == 10) {
-            	this.score += this.ballRolls[this.scoreIndex + 2]
+                this.score += this.ballRolls[this.scoreIndex + 2]
             }
             this.scoreIndex += 2
-		}
-		return this.score;
-		return this.ballRolls.reduce((a,b) => a + b, this.score);
-	};
+        }
+        return this.score;
+    };
 }
 
 
 module.exports = {
-	Game: BowlingGame
+    Game: BowlingGame
 }
